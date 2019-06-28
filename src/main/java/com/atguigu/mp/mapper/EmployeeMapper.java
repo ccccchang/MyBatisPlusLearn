1a589e43231d857aa34de3b619f836cdcfcb803f
tom_glb <goodMorning_glb@atguigu.com>
2019-06-28 08:47:42 +0800

    测试insert方法出现第一个错误id主键没弄

--- src/main/java/com/atguigu/mp/mapper/EmployeeMapper.java
+++ src/main/java/com/atguigu/mp/mapper/EmployeeMapper.java
@@ -3,6 +3,11 @@ package com.atguigu.mp.mapper;
 import com.atguigu.mp.beans.Employee;
 import com.baomidou.mybatisplus.mapper.BaseMapper;
 
+/**
+ * 该接口不需要写任何的实现
+ * @author Administrator
+ *
+ */
 public interface EmployeeMapper extends BaseMapper<Employee>{
 	
 	
--- src/test/java/com/atguigu/mp/test/Test01.java
+++ src/test/java/com/atguigu/mp/test/Test01.java
@@ -6,9 +6,14 @@ import org.junit.Test;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
+import com.atguigu.mp.beans.Employee;
+import com.atguigu.mp.mapper.EmployeeMapper;
+
 public class Test01 {
 
 	private ApplicationContext ioc = new ClassPathXmlApplicationContext("applicationContext.xml");
+	
+	private EmployeeMapper employeeMapper = ioc.getBean("employeeMapper",EmployeeMapper.class);
 	
 	@Test
 	public void testDataSource() throws Exception{
@@ -17,6 +22,22 @@ public class Test01 {
 		
 		System.out.println(dataSource.getConnection());
 		
-		
 	}
+	
+	/**
+	 * 测试插入
+	 */
+	@Test
+	public void insert() {
+//		System.out.println(employeeMapper);
+		
+		Employee employee = new Employee("张三", "zhangsan@qq.com", 1, 15);
+		
+		Integer result = employeeMapper.insert(employee);
+		
+		System.out.println(result);
+	}
+	
+	
+	
 }
